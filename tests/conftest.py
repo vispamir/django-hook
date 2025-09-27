53cd5d336f93f41fc3e52a0932c8761b3d215c53
Amir Koulivand <a.koulivand@shahrzadcity.ir>
2025-09-27 17:20:41 +0330

    Add github workflow

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+import django
+from django.conf import settings
+
+def pytest_configure():
+    if not settings.configured:
+        settings.configure(
+            DEBUG=True,
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': ':memory:',
+                }
+            },
+            INSTALLED_APPS=[
+                'django_hooks',
+            ],
+            SECRET_KEY='test-secret-key-for-django-hooks',
+            USE_TZ=True,
+            TIME_ZONE='UTC',
+        )
+        django.setup()
+
+
+@pytest.fixture(autouse=True)
+def reset_hook_registry():
+    """Reset hook registry before each test"""
+    from django_hooks.registry import hook_registry
+    # Store original state
+    original_hooks = hook_registry._hooks.copy()
+    
+    # Clear for test
+    hook_registry.clear()
+    
+    yield
+    
+    # Restore original state
+    hook_registry._hooks = original_hooks
